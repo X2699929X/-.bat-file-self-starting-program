9a5c88926209e1568525a537288d17282cb947fb
小玄 <71623073+X2699929X@users.noreply.github.com>
2022-10-03 16:20:10 +0800

    Add files via upload

---
+++ RestatClash.bat
@@ -0,0 +1,20 @@
+@echo off  
+  
+echo "killing Clash for Wdinows...."
+  
+taskkill /f /im "Clash for Windows.exe"
+  
+echo "Clash for Winodws Closed."
+  
+
+echo "starting Clash for Windows..."
+
+
+
+start G:\"Clash for Windows"\"Clash for Windows.exe"
+
+echo "Plause waiting  5s"
+
+echo "Success!!"
+	
+exit /b
